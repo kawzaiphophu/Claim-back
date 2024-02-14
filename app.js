3a16d65d9160d1cc580a7c41b8ce9d233e4ae431
BuildTools <unconfigured@null.spigotmc.org>
2024-02-14 21:11:08 +0700

    14

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { PORT, MONGO_HOSTNAME, MONGO_PORT, MONGO_PATH } = process.env;
 const router = require ('./routes/claims')
-const claim = require('./model/claim');
+const Claim = require('./model/claim');
 
 mongoose.connect(`mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_PATH}`)
     .then((e) => console.log("Connected"))
@@ -20,26 +20,15 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const claimSchema = new mongoose.Schema({
-    name: { type: String },
-    tel: { type: String },
-    cTel: { type: String },
-    nameProduct: { type: String },
-    sn: { type: String },
-    symp: { type: String },
-    from: { type: String },
-    update_at: { type: Date, default:Date.now },
-}, { collection: 'list' });
 
-const Claim = mongoose.model('list', claimSchema);
-
-
-
+//list claim all
 app.get("/claimlist", async(req,res)=>{
     const claims = await Claim.find();
     return res.json(claims)
 })
 
+
+//post insert claim
 app.post('/formclaim', async(req, res, next) => {
     try {
         const { name, tel, cTel, nameProduct, sn, symp, from,status } = req.body;
@@ -61,6 +50,25 @@ app.post('/formclaim', async(req, res, next) => {
     }
 
 })
+
+
+app.delete('/claimlist/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      // Find the claim by ID and delete it from the database
+      const deletedClaim = await Claim.findByIdAndDelete(id);
+  
+      if (!deletedClaim) {
+        return res.status(404).json({ message: 'Claim not found' });
+      }
+      return res.status(200).json({ message: 'Claim deleted successfully', deletedClaim: { _id: deletedClaim._id, name: deletedClaim.name } });
+    } catch (error) {
+      console.error('Error deleting claim:', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
 
 
 app.listen(PORT, () => {
--- model/claim.js
+++ model/claim.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose')
 
-const ClaimSchema = new mongoose.Schema({
-    name: String,
-    tel: String,
-    cTel: String,
-    nameProduct: String,
-    sn: String,
-    symp: String,
-    from: String,
-    date: { type: Date, default: Date.now },
-    status : String
-})
-module.exports = mongoose.model('Claim', ClaimSchema)
+// const ClaimSchema = new mongoose.Schema({
+//     name: String,
+//     tel: String,
+//     cTel: String,
+//     nameProduct: String,
+//     sn: String,
+//     symp: String,
+//     from: String,
+//     status : String,
+//     date: { type: Date, default: Date.now }
+// })
+// module.exports = mongoose.model('Claim', ClaimSchema)
+const claimSchema = new mongoose.Schema({
+    name: { type: String },
+    tel: { type: String },
+    cTel: { type: String },
+    nameProduct: { type: String },
+    sn: { type: String },
+    symp: { type: String },
+    from: { type: String },
+    update_at: { type: Date, default:Date.now },
+}, { collection: 'list' });
+
+const Claim = mongoose.model('list', claimSchema);
+module.exports = Claim
